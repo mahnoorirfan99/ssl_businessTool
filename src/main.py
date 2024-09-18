69a972e58c371a5cec76d8349efaca851b34f26b
mahnoorirfan99 <mahnoorirfan1199@gmail.com>
2024-09-17 19:56:07 -0500

    currently working and printing transcript

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from vosk import Model, KaldiRecognizer
+from pydub import AudioSegment
 import streamlit as sl
 import soundfile as sf
 import pandas as pd
 import json
-from pydub import AudioSegment
-import numpy as np
-
+import os
 
 def transcribe_audio(input_audio_path, model_path, buffer_size=4000):
     # Load Vosk model
@@ -17,7 +16,6 @@ def transcribe_audio(input_audio_path, model_path, buffer_size=4000):
     
     # Initialize recognizer with the model and sample rate
     recognizer = KaldiRecognizer(model, 16000)
-    
     
     transcript = ""
     
@@ -33,14 +31,40 @@ def transcribe_audio(input_audio_path, model_path, buffer_size=4000):
     
     return transcript
 
+def save_transcript(transcript, filename):
+     # Define the file path for the transcript
+    transcript_path = os.path.join('transcripts', filename)
+
+    # Write the transcript to the file
+    with open(transcript_path, 'w') as f:
+        f.write(transcript)
+
+    print(f"Transcript saved as: {transcript_path}")
+
+
 def main():
-    input_audio_path = "uploads/tester.wav"
-    model_path = "model/vosk-model-small-en-us-0.15/vosk-model-small-en-us-0.15"
+    sl.title("Audio Transcription Tool")
+    model_path = "model/vosk-model-en-us-0.22-lgraph"
     
-    # Call the transcription function
-    transcript = transcribe_audio(input_audio_path, model_path)
-    print("Transcription:", transcript)
+    uploaded_file = sl.file_uploader("Upload an audio file", type=["wav"])
+
+    if uploaded_file is not None:
+        
+        transcript = transcribe_audio(uploaded_file, model_path)
+
+        # Display transcription result in Streamlit
+        sl.write("Transcription:")
+        sl.text(transcript)
+
+          # Ask the user to input a file name for the transcript
+        transcript_filename = sl.text_input("Enter a name for the transcript file:", "transcript.txt")
+
+        if sl.button("Save Transcript"):
+            # Save the transcript to the 'transcripts' folder
+            save_transcript(transcript, transcript_filename)
+            sl.success(f"Transcript saved as: transcripts/{transcript_filename}")
 
 # Call main function
 if __name__ == "__main__":
     main()
+    
